aa4e8f60764ebe0079f4d230a10b50247412b7b2
Maewa <maewa.dev@gmail.com>
2023-03-24 01:00:26 +0100

    fix menu (close)

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent  implements OnInit {
 
-  constructor() { }
+  constructor(
+    private menu: MenuController
+  ) { }
 
   ngOnInit() {}
 
@@ -32,5 +35,9 @@ export class MenuComponent  implements OnInit {
       icon: '/assets/icon/book.png',
       path: '',
     },
-  ]
+  ];
+
+  close(){
+    this.menu.close()
+  }
 }
